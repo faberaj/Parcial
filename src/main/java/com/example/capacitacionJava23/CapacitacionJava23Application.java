1e3a72b01de5a96b677b0215e0be2ff84d3e1d8c
Manuel Chaparro <manuel_chaparro@gtccorporation.com>
2023-09-23 12:24:31 -0500

    FirstCommit

---
+++ src/main/java/com/example/capacitacionJava23/CapacitacionJava23Application.java
@@ -0,0 +1,13 @@
+package com.example.capacitacionJava23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacionJava23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitacionJava23Application.class, args);
+	}
+
+}
